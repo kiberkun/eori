371d4962214e6e9dfc75de7814ae8c7795812f2e
Alexander Stolyarov <kiberkun@yandex.ru>
2024-08-09 14:31:58 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  
+  devtools: {
+    enabled: true
+  },
+
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=2',
+      htmlAttrs: {
+        lang: 'ru'
+      },
+      meta: [
+        {
+          name: 'MobileOptimized',
+          content: '320'
+        },
+        {
+          name: 'HandheldFriendly',
+          content: 'True'
+        },
+        {
+          'http-equiv': 'cleartype',
+          content: 'on'
+        },
+        {
+          'http-equiv': 'X-UA-Compatible',
+          content: 'ie=edge'
+        }
+      ]
+    }
+  },
+
+  typescript: {
+    shim: false
+  },
+})
